c62fe7bd5f2ae1cf3a0c4f492c8d370f382567fe
SevenSound <875693150@qq.com>
2021-01-04 16:33:01 +0800

    Add swagger template

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
-
+import swaggerUi from 'swagger-ui-express'; 
+import options from './src/config/swagger.json';
 dotenv.config();
 
 const app = express();
@@ -8,6 +9,9 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.static('public'));
+
+// config swagger
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(options));
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
