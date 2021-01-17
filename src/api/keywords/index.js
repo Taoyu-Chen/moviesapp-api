74056d37507ea01c9237f7cdaac7f0aae11c43ef
SevenSound <875693150@qq.com>
2021-01-17 16:40:11 +0800

    fix keywords router

--- src/api/keywords/index.js
+++ src/api/keywords/index.js
@@ -10,7 +10,7 @@ router.get('/', (req, res, next) => {
     .catch((err) => next(err));
 });
 
-router.get('db/:id', (req, res, next) => {
+router.get('/db/:id', (req, res, next) => {
   const id = parseInt(req.params.id);
   getMovieKeywords(id).then(async(keywords) => {
     res.status(200).send(keywords);
@@ -24,4 +24,14 @@ router.get('db/:id', (req, res, next) => {
   }).catch((err) => next(err));
 });
 
+router.get('/test/:id', (req, res, next) => {
+  const id = parseInt(req.params.id);
+  keywordModel.findByKeywordDBId(id).then(keyword => {
+    if (keyword) {
+      return res.status(200).send(keyword);
+    } else {
+      return res.status(404).send({message: `Failed to find keyword with id: ${id}.`, status: 404});
+    }
+  }).catch((err) => next(err));
+});
 export default router;
